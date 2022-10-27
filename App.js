c4b354a40ec4b1078128e0655fa311016df2c861
Zaid Naeem <zaid.naeem@7vals.com>
2022-10-27 18:16:40 +0500

    day end

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Hello Zaid, I am a Doctor ');
+  const [enteredGoal, setGoal] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Hello" onPress={() => setOutputText('You Pressed the button')}/>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Type your Goal" style={styles.input} />
+        <Button title="Press Me" style={{ padding: 5 }} />
+      </View>
+      <View>
+        <Text>{}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#00000',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
+  },
+
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: 15,
+  },
+  input: {
+    width: 220,
+    borderBottomColor: "black",
+    borderBottomWidth: 3,
+    padding: 3,
   },
 });
